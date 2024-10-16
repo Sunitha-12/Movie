1cdb4aa77e89e190be8f6db21b5e3b413a90e10f
unknown <nsunitha.cse.rymec@gmail.com>
2024-10-16 14:40:44 +0530

    movie

---
+++ README.md
@@ -0,0 +1,2 @@
+to add the tmdb images we need to add the additional url in front of it 
+--------->https://image.tmdb.org/t/p/orginal/${x.backdrop_pathg}
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import React from "react"
+import Parts from "./Parts"
+import Infopage from "./pages/Infopage"
+const App = () => {
+ return(
+  <>
+  <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<Parts/>}></Route>
+  <Route path="/inner" element={<Infopage/>}></Route>
+  </Routes>
+  </BrowserRouter>
+  </>
+ )
+    
+    
+}
+
+export default App
